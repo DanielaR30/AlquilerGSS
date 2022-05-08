e06d2cfb5e980c741903ccfdf98f30c065025dca
DanielaR30 <droncancio3@misena.edu.co>
2022-05-07 23:51:35 -0500

    .
    .

---
+++ Controllers/CarroesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AlquilerGSS.Models;
+
+namespace AlquilerGSS.Controllers
+{
+    public class CarroesController : Controller
+    {
+        private readonly GSSContext _context;
+
+        public CarroesController(GSSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Carroes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Carros.ToListAsync());
+        }
+
+        // GET: Carroes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carro = await _context.Carros
+                .FirstOrDefaultAsync(m => m.Idcarro == id);
+            if (carro == null)
+            {
+                return NotFound();
+            }
+
+            return View(carro);
+        }
+
+        // GET: Carroes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Carroes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Idcarro,Placa,Marca,Modelo,Costo,Disponible")] Carro carro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(carro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(carro);
+        }
+
+        // GET: Carroes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carro = await _context.Carros.FindAsync(id);
+            if (carro == null)
+            {
+                return NotFound();
+            }
+            return View(carro);
+        }
+
+        // POST: Carroes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Idcarro,Placa,Marca,Modelo,Costo,Disponible")] Carro carro)
+        {
+            if (id != carro.Idcarro)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(carro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CarroExists(carro.Idcarro))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(carro);
+        }
+
+        // GET: Carroes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carro = await _context.Carros
+                .FirstOrDefaultAsync(m => m.Idcarro == id);
+            if (carro == null)
+            {
+                return NotFound();
+            }
+
+            return View(carro);
+        }
+
+        // POST: Carroes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var carro = await _context.Carros.FindAsync(id);
+            _context.Carros.Remove(carro);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CarroExists(int id)
+        {
+            return _context.Carros.Any(e => e.Idcarro == id);
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# AlquilerGSS
- Prueba
